7c90c8fce205609faa890e906ffae01968b8596f
Dmitry Sterlin <uglenpish@rambler.ru>
2026-05-08 16:38:16 +0300

    Added contacts shema

---
+++ backend/prisma/migrations/20260508125902_add_contact_messages/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "ContactMessage" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'new',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ContactMessage_pkey" PRIMARY KEY ("id")
+);
